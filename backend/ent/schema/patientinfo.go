07a278d549fb8ac10c0ea5dde1fce245f6972b38
Thanat <72113322+Thanatpp@users.noreply.github.com>
2020-12-24 21:55:43 +0700

    เพิ่ม entity patientinfo(ข้อมูลผู้ป่วย) - close #46

--- backend/ent/schema/patientinfo.go
+++ backend/ent/schema/patientinfo.go
@@ -17,20 +17,14 @@ func (PatientInfo) Fields() []ent.Field {
 		field.String("cardNumber").NotEmpty().Unique(),
 		field.String("name").NotEmpty(),
 		field.String("surname").NotEmpty(),
+		field.String("gender").NotEmpty(),
 		field.Int("age").Positive(),
-		field.String("address"),
-		field.String("chronicDisease"),
-		field.String("congenitalDisease"),
-		field.String("drugAllergy"),
 	}
 }
 
 // Edges of the PatientInfo.
 func (PatientInfo) Edges() []ent.Edge {
 	return []ent.Edge{
-		edge.From("nametitle", NameTitle.Type).Ref("patientinfo").Unique().Required(),
-		edge.From("gender", Gender.Type).Ref("patientinfo").Unique().Required(),
-		edge.From("bloodtype", BloodType.Type).Ref("patientinfo").Unique().Required(),
-		edge.To("medicalhistorys", MedicalHistory.Type).StorageKey(edge.Column("owner_id")),
+		edge.To("drugallergys", DrugAllergy.Type).StorageKey(edge.Column("patient_id")),
 	}
 }
